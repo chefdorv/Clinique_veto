9234180cb93d8d03921da95790d57703c49c9fd5
Ewan <penanchoat@gmail.com>
2018-07-02 09:40:52 +0200

    Ajout libs

--- src/DAL/AppliTestDAL.java
+++ src/DAL/AppliTestDAL.java
@@ -1,7 +1,32 @@
 package DAL;
 
-public class AppliTestDAL {
-    public static void main(String[] args) {
+import java.sql.SQLException;
 
-    }
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class AppliTestDAL {
+
+	public static void main(String[] args) throws DALException {
+
+		Connection con = null;
+		PreparedStatement rqt = null;
+		ResultSet rs = null;
+
+		try {
+			con = JdbcTools.getConnection();
+
+			rqt = con.prepareStatement("select * from Clients");
+			rs = rqt.executeQuery();
+
+			if (rs.next()) {
+				System.out.println(rs.getString("NomClient"));
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
