cdfa9e06fde3458c3767f01bb0283ad0bd6b5650
Ewan <penanchoat@gmail.com>
2018-07-02 09:46:08 +0200

    ouyéé

--- src/IHM/EcranConnexion.java
+++ src/IHM/EcranConnexion.java
@@ -15,9 +15,6 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class EcranConnexion extends javax.swing.JFrame {
-
-
-    private LoginController lg;
     
     public EcranConnexion() {
         initComponents();
